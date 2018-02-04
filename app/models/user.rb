41776fb3e8a88e7eb87cf4492960234d249a26a2
Shoabe Shariff <shoabe.shariff@gmail.com>
2018-02-03 19:38:21 -0800

    Uses random UUID as User ID

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
   has_one :social_media_link
   attr_accessor :first_name, :last_name, :email, :password
+  before_create :generate_random_id
 
   validates :first_name, presence: true, length: { maximum: 40 }
   validates :last_name, presence: true, length: { maximum: 40 }
+
+  private
+
+  def generate_random_id
+    self.id = SecureRandom.uuid
+  end
 end
